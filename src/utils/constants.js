e8a41a5bd20615702af914f74660d52c564b2abe
syedasadhashmi <asadhashmi22@gmail.com>
2025-11-25 00:43:20 +0500

    globally set BASE_URL

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,8 @@
-export const BASE_URL = "/api";
+// For Production
+
+// export const BASE_URL = "/api";
+
+// Dor development
+
+export const BASE_URL =
+  location.hostname === "localhost" ? "http://localhost:7777/api" : "/api";
